3be2c92303dbe14de23796061e73cf8c1c3d7fb5
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 12:41:16 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string="This is a test.")
+  reversed = []
+  string_ary = %w(string)
+  reversed = string_ary.collect {|i| i.reverse}
+  puts reversed
+  reversed.join("")
+  puts reversed
+end
