e8aeb36fdd7b598700ae30c3605d90ae1ef07065
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 12:59:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string="This is a test.")
   reversed = []
-  string_ary = %w{string}
+  string_ary = %w(#{string})
   reversed = string_ary.collect {|i| i.reverse}
   puts reversed
   reversed.join("")
