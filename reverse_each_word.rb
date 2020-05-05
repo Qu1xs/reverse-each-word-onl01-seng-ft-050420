9526791b4ab41d1172de5de0d9b31f0b6bcba428
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 13:43:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(string="This is a test.")
   reversed = []
   string_ary = string.split(" ")
-  reversed = string_ary.collect {|i| "#{.reverse}"}
+  reversed = string_ary.collect {|i| "#{i.reverse}"}
   binding.pry
   reversed.join(" ")
   binding.pry
