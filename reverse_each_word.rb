5f4d16e52e84e018ac308dcbd51719a2bb1f62ba
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 13:15:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string="This is a test.")
   reversed = []
   string_ary = string.split(" ")
   reversed = string_ary.collect {|i| i.reverse}
-  reversed.join("", """)
+  reversed.join("", "")
   return reversed
 end
